958ddfb075c5a15c9cf5fa20f705ffd07c94557a
Rodrigo Torres <rodrigo@zirian.com>
2020-07-14 15:39:11 -0500

    Se hizo logout

--- resources/views/logout.blade.php
+++ resources/views/logout.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Login
+                    Logout
                 </div>
 
                 <div class="links">
