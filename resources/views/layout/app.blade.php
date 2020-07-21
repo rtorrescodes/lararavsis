099ee9082423cb31b1f8951c30d4d4cf565213b7
Rodrigo Torres <rodrigo@zirian.com>
2020-07-21 11:14:08 -0500

    escribi side en app.blade.php

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         @section('sidebar')
-            This is the master sidebar.
+            This is the master sidesidebar.
         @show
 
         <div class="container">
